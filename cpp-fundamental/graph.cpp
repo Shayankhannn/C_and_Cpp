34ff77993d30a83b8749bf5fb64f5dbaecd4d83a
shayankhannn <shayannkhannn007@gmail.com>
2026-05-13 03:06:26 -0700

    djikstra shortest path algo

---
+++ cpp-fundamental/graph.cpp
@@ -0,0 +1,12 @@
+// djikstra shortest path using graph data structure
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<pair int,int>graph[5];
+    graph[0] = {2,4};
+
+}
