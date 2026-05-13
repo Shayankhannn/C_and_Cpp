4ebd165347441a35c0bb1f444a66c028fcca2aca
shayankhannn <shayannkhannn007@gmail.com>
2026-05-13 06:54:11 -0700

    point custom class and overloading

---
+++ cpp-fundamental/point.cpp
@@ -0,0 +1,25 @@
+
+point operator+ (point &p1,point &p2){
+    point sum = {p1.x+p2.x ,p1.y+p2.y}
+    return sum;
+}
+
+ostream& operator<< (ostream& out , const point& p){
+    out << "(" << p.x << "," << p.y << ")"
+;
+return out ;
+}
+
+int main (){
+    point a ={3.5,4.5} ,b = {2.5,6.3};
+    cout << "a=" << a << "b=" << b << endl ;
+    cout << "sum = " << a + b  << endl ;
+}
+
+class point {
+    public :
+    double getx(){return x;} // accessor
+    void setx (double v){x=v;} // mutator
+}
+
+// this is all just operator overlaoding and class c++ doesnt know how to compute custom class so he just compute based on what you provide so we create custom + and cout 
